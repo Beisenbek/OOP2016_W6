e1c15b6961d26df8b023b680b240181b7ab023a5
Beisenbek M. Baisakov <b.baisakov@kbtu.kz>
2016-09-26 19:27:57 +0600

    init

---
+++ Singleton/Singleton/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Singleton
+{
+    public class A
+    {
+        public static Hero h;
+    }
+
+    public class Hero
+    {
+        private string id;
+        private static Hero _hero;
+        private Hero()
+        {
+            id = Guid.NewGuid().ToString();
+        }
+        public string Id { get { return this.id; } }
+
+        static public Hero Instance()
+        {
+            if (_hero == null)
+            {
+                _hero = new Hero();
+            }
+            return _hero;
+        }
+    }
+
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Hero mario = Hero.Instance();
+            Hero mario2 = Hero.Instance();
+
+            Console.WriteLine(mario.Id);
+            Console.WriteLine(mario2.Id);
+        }
+    }
+}
---
+++ StaticProperty/StaticProperty/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StaticProperty
+{
+    class A
+    {
+        public static string property = Guid.NewGuid().ToString();
+        public static string property2 = Guid.NewGuid().ToString();
+        public static string property3 = Guid.NewGuid().ToString();
+        public static string property4 = Guid.NewGuid().ToString();
+        public static string property5 = Guid.NewGuid().ToString();
+        public static string property6 = Guid.NewGuid().ToString();
+    }
+
+    class B : A
+    {
+        public void DoIt()
+        {
+            Console.WriteLine("ok");
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(A.property);
+            Console.WriteLine(A.property);
+            Console.WriteLine(A.property);
+            B b = new B();
+            b.DoIt();
+            A a = new A();
+        }
+    }
+}
